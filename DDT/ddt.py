d2f98e131828befc8c79358ff00889fdac92c5a4
Aref TB <92437542+ArefMYTB@users.noreply.github.com>
2023-08-28 23:35:55 +0330

    tune input & output

--- DDT/ddt.py
+++ DDT/ddt.py
@@ -12,11 +12,10 @@ def load_model():
     return model
 
 
-# get the distorted image and a corresponding mask
-def ddt(image, mask):
-    model = load_model()
+def classify(model, img):
     with torch.no_grad():
-        output = model(image.unsqueeze(0), mask.unsqueeze(0))  # Add batch dimension
+        # output = model(image.unsqueeze(0), mask.unsqueeze(0))  # Add batch dimension
+        output = model(img)
         probabilities = torch.softmax(output, dim=1)
         predicted_class = torch.argmax(probabilities, dim=1).item()
 
@@ -24,6 +23,36 @@ def ddt(image, mask):
     predicted_label = class_labels[predicted_class]
 
     print(f'Predicted class: {predicted_label}')
+    return predicted_label, probabilities
+
+
+def distorted_info():
+    print("Distorted function called")
+    return []
+
+
+def texture_info():
+    print("Texture function called")
+    return []
+
+
+def none_info():
+    print("None function called")
+    return []
+
+
+# get the distorted image and a corresponding mask
+def ddt(img):
+    model = load_model()
+    predicted_label, probabilities = classify(model, img)
+
+    if predicted_label == ['None']:
+        none_information = none_info()
+        return none_information, probabilities
+    else:
+        distorted_information = distorted_info()
+        texture_information = texture_info()
+        return [distorted_information, texture_information], probabilities
 
 
 def main():
@@ -38,9 +67,12 @@ def main():
     image = preprocess(image)
     mask = preprocess(mask)
 
-    ddt(image, mask)
+    # concat image & mask
+    fused_features = torch.cat((image, mask), dim=1)
+    # fused_features = fused_features.view(fused_features.size(0), -1)  # Flatten
+
+    ddt(fused_features)
 
 
 if __name__ == '__main__':
     main()
-
