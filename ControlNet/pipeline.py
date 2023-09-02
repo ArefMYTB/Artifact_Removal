5a99e9ce8f65564796974c1076036a25193d1cc3
Aref TB <92437542+ArefMYTB@users.noreply.github.com>
2023-09-02 16:25:19 +0330

    go with 2 condition

--- ControlNet/pipeline.py
+++ ControlNet/pipeline.py
@@ -15,7 +15,7 @@ def load_controlnet():
     # TODO load other condition pretraiend model
     # controlnet3 = []
     # controlnet4 = []
-    controlnet = [controlnet1, controlnet2, controlnet1, controlnet2]
+    controlnet = [controlnet1, controlnet2]
 
     # controlnet pipeline
     controlNetInpaintPipeline = StableDiffusionControlNetInpaintPipeline.from_pretrained(
