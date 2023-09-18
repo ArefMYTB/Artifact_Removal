ac951f53711970fe7d04ff850986c450a6f6851d
Aref TB <92437542+ArefMYTB@users.noreply.github.com>
2023-09-18 05:10:19 +0330

    add mask using attention

--- DDT/model.py
+++ DDT/model.py
@@ -1,119 +1,82 @@
-import torch
-import torchvision.models as models
-import torch.nn as nn
-import torch.optim as optim
 import yaml
 from tqdm import tqdm
-from torchvision.models import ResNet50_Weights
-# from data_prepare import get_dataloaders
+# from torchvision.models import ResNet50_Weights
+import torch
+import torch.nn as nn
+from torchvision import models
+import torch.optim as optim
+from torchvision import transforms
+import timm  # This is for Vision Transformer (ViT)
+import cv2
+from PIL import Image
+import numpy as np
+
+class CoefficientGenerator(nn.Module):
+    def __init__(self, num_output=3):
+        super(CoefficientGenerator, self).__init__()
+        
+        # Initialize a pre-trained ResNet-100 model
+        self.resnet100 = models.resnet101(pretrained=True)
+        
+        # TODO get this from config
+        freeze_layer_number = 70
+        # Freeze the first layers
+        self.freeze_layers(freeze_layer_number)
+        
+        # TODO get this from config
+        img_size = 512
+        # Initialize Vision Transformer (ViT)
+        self.vit = timm.create_model('vit_base_patch16_224', img_size=img_size, pretrained=True)
+
+        # Define attention mechanism for combining image and mask
+        self.attention = nn.MultiheadAttention(embed_dim=1000, num_heads=8)
+
+        # Output layer
+        self.fc = nn.Linear(1000, num_output)
+
+    def freeze_layers(self, num):
+        self.resnet_layers = nn.Sequential(*list(self.resnet100.children())[:num])
+        for param in self.resnet_layers.parameters():
+            param.requires_grad = False
+
+    def forward(self, image, mask):
+      
+        image_features = self.resnet100(image)
+
+        # Expand the mask to match the number of channels in the image
+        mask = mask.expand_as(image)
+
+        mask_features = self.vit(mask)
+
+        # Apply attention mechanism to combine image and mask features
+        combined_features, _ = self.attention(image_features, mask_features, mask_features)
+
+        output = self.fc(combined_features)
+
+        return output
 
 
-def get_model():
-    # Load the pre-trained ResNet model
-    model = models.resnet50(weights=ResNet50_Weights.DEFAULT)
+if __name__ == "__main__":
+  
+    generator = CoefficientGenerator(num_output=3)
+    
+    # image = torch.randn(1, 3, 512, 512)
+    # mask = torch.randn(1, 1, 512, 512)  
+    image_path = 'distorted.jpg'
+    mask_path = 'mask.jpg'
 
-    # Modify the number of the input channels
-    weight = model.conv1.weight.clone()
-    model.conv1 = nn.Conv2d(4, 64, kernel_size=7, stride=2, padding=3, bias=False)
-    with torch.no_grad():
-        model.conv1.weight[:, :3] = weight
-        model.conv1.weight[:, 3] = model.conv1.weight[:, 0]
+    image = Image.open(image_path).convert('RGB')
+    transform = transforms.Compose([
+        transforms.Resize((512, 512)),
+        transforms.ToTensor()
+    ])
+    image = transform(image)
+    image = image.unsqueeze(0) 
 
-    # Modify the last layer to represent the 4 coefficients
-    num_classes = 4
-    model.fc = nn.Linear(model.fc.in_features, num_classes)
+    mask = Image.open(mask_path)
+    mask = mask.convert('L') 
+    mask = transform(mask)
+    mask = mask.unsqueeze(0)  
 
-    return model
-
-# # Read the necessary parameters from the config file
-# with open("config.yml", 'r') as file:
-#     conf = yaml.safe_load(file)["model"]
-#
-# Pretrain_PATH = conf["Pretrain_PATH"]
-#
-# num_epochs = conf["num_epochs"]
-#
-# label_mapping = {'Deformation': 0, 'Texture': 1, 'None': 2}
-#
-# # Load the pre-trained ResNet model
-# model = models.resnet50(weights=ResNet50_Weights.DEFAULT)
-#
-# # Modify the number of the input channels
-# weight = model.conv1.weight.clone()
-# model.conv1 = nn.Conv2d(4, 64, kernel_size=7, stride=2, padding=3, bias=False)
-# with torch.no_grad():
-#     model.conv1.weight[:, :3] = weight
-#     model.conv1.weight[:, 3] = model.conv1.weight[:, 0]
-#
-# # Modify the last layer to represent the 4 coefficients
-# num_classes = 4
-# model.fc = nn.Linear(model.fc.in_features, num_classes)
-#
-# # summary(model, (4, 224, 224))
-#
-# # Load and preprocess the dataset
-# train_loader, val_loader, test_loader = get_dataloaders()
-#
-# # Define loss function and optimizer
-# criterion = nn.CrossEntropyLoss()
-# optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-#
-# # Training #######################################
-# for epoch in tqdm(range(num_epochs)):
-#     for batch in train_loader:
-#         distorted_images = batch['distorted']
-#         mask_images = batch['mask']
-#
-#         labels = batch['label']
-#         labels = [label_mapping[label] for label in labels]
-#         labels = torch.tensor(labels)
-#
-#         inputs = []
-#         for distorted_image, mask_image in zip(distorted_images, mask_images):
-#             inputs.append(torch.cat((distorted_image, mask_image), dim=0))
-#         inputs = torch.stack(inputs)
-#
-#         outputs = model(inputs)
-#
-#         loss = criterion(outputs, labels)
-#
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-#
-#     torch.save(model, Pretrain_PATH)
-#
-# # Evaluation #####################################
-# # Set the model to evaluation mode
-# model.eval()
-#
-# # Initialize evaluation variables
-# total_samples = 0
-# correct_predictions = 0
-#
-# # Disable gradients for evaluation
-# with torch.no_grad():
-#     for batch in val_loader:
-#         distorted_images = batch['distorted']
-#         mask_images = batch['mask']
-#
-#         labels = batch['label']
-#         labels = [label_mapping[label] for label in labels]
-#         labels = torch.tensor(labels)
-#
-#         inputs = []
-#         for distorted_image, mask_image in zip(distorted_images, mask_images):
-#             inputs.append(torch.cat((distorted_image, mask_image), dim=0))
-#         inputs = torch.stack(inputs)
-#
-#         # Forward pass and prediction
-#         outputs = model(inputs)
-#         _, predicted_labels = torch.max(outputs, 1)
-#
-#         # Update evaluation variables
-#         total_samples += labels.size(0)
-#         correct_predictions += (predicted_labels == labels).sum().item()
-#
-# # Calculate accuracy
-# accuracy = correct_predictions / total_samples
-# print(f"Accuracy: {accuracy}")
+    coefficients = generator(image, mask)
+    print("Generated Coefficients:", coefficients)
