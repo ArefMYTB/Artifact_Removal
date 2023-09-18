dc76114d01cc3beeba1b3de030ef6f1f38fdf0c9
Aref TB <92437542+ArefMYTB@users.noreply.github.com>
2023-09-18 18:28:04 +0330

    simplify condition generation

--- DDT/ddt.py
+++ DDT/ddt.py
@@ -1,128 +1,25 @@
-import torch
-import torchvision.transforms as transforms
-from PIL import Image
-import numpy as np
-import yaml
-from controlnet_aux import OpenposeDetector, HEDdetector
+from controlnet_aux.processor import Processor
 
-# Read the necessary parameters from the config file
-with open("config.yml", 'r') as file:
-    conf = yaml.safe_load(file)
+# load processor from processor_id
+# options are:
+# ["canny", "depth_leres", "depth_leres++", "depth_midas", "depth_zoe", "lineart_anime",
+#  "lineart_coarse", "lineart_realistic", "mediapipe_face", "mlsd", "normal_bae", "normal_midas",
+#  "openpose", "openpose_face", "openpose_faceonly", "openpose_full", "openpose_hand",
+#  "scribble_hed, "scribble_pidinet", "shuffle", "softedge_hed", "softedge_hedsafe",
+#  "softedge_pidinet", "softedge_pidsafe", "dwpose"]
+def controlnet_condition(img, processor_id):
+    processor = Processor(processor_id)
 
-Pretrain_PATH = conf["model"]["Pretrain_PATH"]
-label_mapping = {'HED': 0, 'Pose': 1, 'Texture': 2, 'Reference': 3}
-
-
-def load_model():
-    # Load pre-trained model
-    model = torch.load(Pretrain_PATH)
-    model.eval()  # Set the model to evaluation mode
-    return model
+    processed_image = processor(img, to_pil=True)
+    return processed_image
 
 
-# Use DDT Classifier
-def classify(model, img):
-    with torch.no_grad():
-        output = model(img)
-        _, predicted_labels = torch.max(output, 1)
-
-    class_labels = ['Distorted', 'Pose', 'Texture', 'Reference']
-    predicted_label = class_labels[predicted_labels]
-
-    print(f'Predicted class: {predicted_label}')
-    return predicted_label, output
-
-
-def hed_condition(img):
-    print("hed_condition called")
-
-    hed = HEDdetector.from_pretrained('lllyasviel/ControlNet')
-
-    hed_image = hed(img)
-
-    return hed_image
-
-
-def canny_condition(img):
-    canny_img_np = np.array(img)
-    # get canny image
-    canny_image = cv2.Canny(canny_img_np, 100, 200)
-    canny_image = canny_image[:, :, None]
-    canny_image = np.concatenate([canny_image, canny_image, canny_image], axis=2)
-
-    canny_image = Image.fromarray(canny_image)
-
-    return canny_image
-
-
-def pose_condition(img):
-    print("pose_condition called")
-
-    openpose = OpenposeDetector.from_pretrained('lllyasviel/ControlNet')
-
-    pose_real_image = img
-
-    pose_image = openpose(pose_real_image)
-    # pose_real_image = pose_real_image.resize(pose_image.size)
-
-    # pose_mask = np.zeros_like(np.array(pose_image))
-    # pose_mask[250:700, :, :] = 255
-    # pose_mask = Image.fromarray(pose_mask)
-
-    return pose_image
-
-
-def texture_condition():
-    print("texture_condition called")
-    return []
-
-
-def reference_condition(reference_img):
-    print("reference_condition called")
-    return []
-
-
-# get the distorted image and a corresponding mask
 def ddt(img, reference_img=None):
-    # model = load_model()
-    # predicted_label, probabilities = classify(model, img)
+    canny_image = controlnet_condition(img, "canny")
+    hed_image = controlnet_condition(img, "softedge_hed")
+    lineart_realistic = controlnet_condition(img, "lineart_realistic")
+    normal_bae = controlnet_condition(img, "normal_bae") # little slow
+    pose_image = controlnet_condition(img, "openpose_full")
+    texture_image = reference_img
 
-    # if predicted_label == label_mapping['None']:
-    #     none_information = none_info()
-    #     return none_information, probabilities
-    # else:
-    #     deformation_information = deformation_info()
-    #     texture_information = texture_info()
-    #     return [deformation_information, texture_information], probabilities
-
-    # TODO there's a problem with condtions loading
-    return [canny_condition(img), pose_condition(img)]
-    # return [hed_condition(img), pose_condition(img), texture_condition(), reference_condition(reference_img)]
-
-
-def main():
-    # Load image and mask
-    image = Image.open('distorted.png')
-    mask = Image.open('mask_1.png').convert('L')
-
-    input_width = conf["data"]["input_width"]
-    input_height = conf["data"]["input_height"]
-
-    # Define transformation to be applied to the images
-    preprocess = transforms.Compose([
-        transforms.Resize((input_width, input_height)),
-        transforms.ToTensor()
-    ])
-
-    image = preprocess(image)
-    mask = preprocess(mask)
-
-    # concat image & mask
-    fused_features = torch.cat((image, mask), dim=0)
-    fused_features = torch.stack([fused_features])
-
-    ddt(fused_features)
-
-
-if __name__ == '__main__':
-    main()
+    control_images = [canny_image, pose_image, texture_image]
