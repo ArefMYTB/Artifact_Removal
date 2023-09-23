3b235acc0cb8b0b0b5105c339e03bebd9aefb2b8
Negar Movaghatian <movaghatian.negar@gmail.com>
2023-09-23 12:09:37 +0330

    Update ddt.py to get batches

--- DDT/ddt.py
+++ DDT/ddt.py
@@ -7,6 +7,7 @@ from controlnet_aux.processor import Processor
 #  "openpose", "openpose_face", "openpose_faceonly", "openpose_full", "openpose_hand",
 #  "scribble_hed, "scribble_pidinet", "shuffle", "softedge_hed", "softedge_hedsafe",
 #  "softedge_pidinet", "softedge_pidsafe", "dwpose"]
+
 def controlnet_condition(img, processor_id):
     processor = Processor(processor_id)
 
@@ -14,12 +15,18 @@ def controlnet_condition(img, processor_id):
     return processed_image
 
 
-def ddt(img, reference_img=None):
-    canny_image = controlnet_condition(img, "canny")
-    hed_image = controlnet_condition(img, "softedge_hed")
-    lineart_realistic = controlnet_condition(img, "lineart_realistic")
-    normal_bae = controlnet_condition(img, "normal_bae") # little slow
-    pose_image = controlnet_condition(img, "openpose_full")
-    texture_image = reference_img
+def ddt(img_batch, reference_img=None):
+    control_batch = []
+    for i, img in enumerate(img_batch):
+        canny_image = controlnet_condition(img[0], "canny")
+        hed_image = controlnet_condition(img[0], "softedge_hed")
+        lineart_realistic = controlnet_condition(img[0], "lineart_realistic")
+        normal_bae = controlnet_condition(img[0], "normal_bae") # little slow
+        pose_image = controlnet_condition(img[0], "openpose_full")
+        texture_image = reference_img[i]
+        
+        control_images = [canny_image, hed_image, lineart_realistic, normal_bae, pose_image, texture_image]
 
-    control_images = [canny_image, pose_image, texture_image]
+        control_batch.append(control_images)
+    
+    return control_batch
